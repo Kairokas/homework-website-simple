4d1a70094b06e476f08682b76790691b55278e07
Kairokas <kairo9@tlu.ee>
2022-04-05 18:09:47 +0300

    Responsiveness + seperate files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Get the button:
 mybutton = document.getElementById("top-btn");
-console.log(mybutton);
+//console.log(mybutton);
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
