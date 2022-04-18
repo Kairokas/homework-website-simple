f1b7cdc62f46a56d00ee6b8a0db5fedba7e2f890
Kairokas <kairo9@tlu.ee>
2022-04-18 18:44:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Get the button:
 mybutton = document.getElementById("top-btn");
 //console.log(mybutton);
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 30px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
